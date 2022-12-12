58e16b8cbd6f5e3834f017a6301721858a43e1f4
Ivy Wang <iwang@princeton.edu>
2022-12-12 17:40:48 -0500

    Add head and arms

--- src/components/objects/Figure/Figure.js
+++ src/components/objects/Figure/Figure.js
@@ -1,17 +1,29 @@
-class Figure {
-    constructor() {
-    }
+import { Group } from 'three';
 
-    createBody() {
-        this.body = new THREE.Group();
+// Reference: https://tympanus.net/codrops/2021/10/04/creating-3d-characters-in-three-js/
+class Figure extends Group{
+    constructor() {
+        // Create body as a box
         // Create a material with a white color
         const material = new THREE.MeshLambertMaterial({ color: 0xffffff });
-        const geometry = new THREE.BoxGeometry(1, 1.5, 1);
-        const mesh = new THREE.Mesh(geometry, material);
+        const bodyGeometry = new THREE.BoxGeometry(1, 1.5, 1);
+        const mesh = new THREE.Mesh(bodyGeometry, material);
         this.add(mesh);
-    }
 
-    init() {
-        this.createBody();
+        // Create head
+        const headGeometry = new THREE.BoxGeometry(1.4, 1.4, 1.4)
+        const head = new THREE.Mesh(headGeometry, material)
+        this.add(head)
+        
+        // Position it above the body
+        head.position.y = 1.65
+
+        // Create arms
+        for (let i = 0; i < 2; i++) {
+            const geometry = new THREE.BoxGeometry(0.25, 1, 0.25)
+            const arm = new THREE.Mesh(geometry, material)
+            
+            this.add(arm)
+        }
     }
 }
